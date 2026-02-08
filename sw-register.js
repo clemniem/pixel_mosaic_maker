61f6ad9eb6ec30711ad4d024d437307e45a6bd4a
clemniem <clemens.niemeyer@gmail.com>
2026-02-08 16:06:17 +0100

    fix MIME type error

--- sw-register.js
+++ sw-register.js
@@ -1,4 +1,5 @@
-if ('serviceWorker' in navigator) {
+// Only register the service worker in production (avoids MIME/404 issues on local dev)
+if ('serviceWorker' in navigator && location.hostname !== 'localhost') {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register(new URL('./sw.js', import.meta.url).href).catch(() => {});
   });
